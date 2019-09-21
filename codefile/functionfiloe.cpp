3454352a55b75de960474f98d8f6beef5abbb98a
xiang <1069342827@qq.com>
2019-09-21 20:11:55 +0800

    函数的使用

---
+++ codefile/functionfiloe.cpp
@@ -0,0 +1,10 @@
+//
+// Created by xiang on 2019/9/21.
+//
+
+#include "../headerfile/functionfiloe.h"
+//函数的分文件编写--让代码结构更清晰
+int function_file(int a,int b){
+
+    return  a + b;
+}
---
+++ headerfile/functionfiloe.h
@@ -0,0 +1,19 @@
+//
+// Created by xiang on 2019/9/21.
+//
+
+#ifndef CBUTY_FUNCTIONFILOE_H
+#define CBUTY_FUNCTIONFILOE_H
+
+
+/**
+ * 函数的分文件编写--让代码结构更清晰
+ */
+class functionfiloe {
+
+};
+
+//函数的分文件编写--让代码结构更清晰
+int function_file(int a,int b);
+
+#endif //CBUTY_FUNCTIONFILOE_H
